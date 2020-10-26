f5f9b11b5113a9dae4a34239569f8e8c0821c9a5
Externoak <dariomr1234@gmail.com>
2020-10-26 23:33:18 +0100

    Added tooltip to buttons, check if MSFS is currently running, remind already selected folder so when pressing cancel when choosing a folder it does not forget it, fixed bug with unzip threading and if unzip fails reason is now stated.

--- Downloader.py
+++ Downloader.py
@@ -9,13 +9,14 @@ from PIL.ImageTk import PhotoImage
 from hurry.filesize import size
 from tkinter.ttk import *
 from tkinter import filedialog, ttk
+import psutil as psutil
 import requests
 import tqdm
 import zipfile
 import sys
 import json
 
-current_version = 'v0.5.1'
+current_version = 'v0.6'
 installer_release_url = 'https://api.github.com/repos/externoak/A32NX-installer/releases/latest'
 master_prerelease_url = 'https://api.github.com/repos/flybywiresim/a32nx/releases/tags/vmaster'
 latest_release_url = 'https://api.github.com/repos/flybywiresim/a32nx/releases/latest'
@@ -97,10 +98,60 @@ class Request:
         webbrowser.open(url='https://github.com/Externoak/A32NX-installer/releases/latest')
 
 
+class CreateToolTip(object):
+
+    def __init__(self, widget, text='widget info'):
+        self.waittime = 500
+        self.wraplength = 180
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
+
+    def enter(self, _event=None):
+        self.schedule()
+
+    def leave(self, _event=None):
+        self.unschedule()
+        self.hidetip()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+
+    def unschedule(self):
+        current_id = self.id
+        self.id = None
+        if current_id:
+            self.widget.after_cancel(current_id)
+
+    def showtip(self):
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 50
+        y += self.widget.winfo_rooty() + 35
+        self.tw = tkinter.Toplevel(self.widget)
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = ttk.Label(self.tw, text=self.text, justify='left',
+                          background="#1B1B1B", relief='solid', borderwidth=1,
+                          wraplength=self.wraplength)
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tw
+        self.tw = None
+        if tw:
+            tw.destroy()
+
+
 class Application(ttk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
+        master.title(f'FlyByWire Downloader {current_version}')
         self.change_folder = True
         self.original_background = ""
         self.Artwork = ttk.Label(image="", borderwidth=0)
@@ -108,62 +159,67 @@ class Application(ttk.Frame):
         self.Artwork['image'] = self.photo
         self.Artwork.photo = self.photo
         self.Artwork.pack()
-        self.response_status = ttk.Label(text="Welcome to A32NX Mod Downloader & Installer!", wraplength=400, background="#1B1B1B", foreground="white")
-        self.response_status.pack(side="top", fill=tkinter.X)
-        self.update_installer_label = ttk.Label(text="", background="#1B1B1B")
-        threading.Thread(target=self.check_installer_update()).start()
-        self.filler_label = ttk.Label(text="", background="#1B1B1B")
-        self.filler_label.pack(side="top", fill=tkinter.X)
-        self.destination_folder_msg = ttk.Label(text="", background="#1B1B1B", wraplength=400)
-        self.destination_folder_msg.pack(side="top", fill=tkinter.X)
-        self.progress_bar = ttk.Progressbar(self, style='text.Horizontal.TProgressbar', orient="horizontal", length=200, mode="determinate")
-        self.download_dev_btn = ttk.Button(self, text="Development version", width=20, style='W1.TButton', command=self.download_dev)
-        self.download_stable_btn = ttk.Button(self, width=20, text="Stable version", style='W2.TButton', command=self.download_stable)
-        self.browse_button = ttk.Button(self, text="", width=30, style='W3.TButton', command=self.browse_search)
-        self.exit = ttk.Button(self, text="Exit", style='W4.TButton', command=self.master.destroy)
-        self.exit.pack(side="bottom", pady=(30, 0), padx=(184, 184))
-        self.filler_label2 = ttk.Label(text="", background="#1B1B1B")
-        self.filler_label2.pack(side="bottom", fill=tkinter.X)
-        self.cancel = ttk.Button(self, text="Cancel", style='W5.TButton', command=Request.update_cancel)
-        root.bind_class("TButton", "<Enter>", self.on_enter)
-        root.bind_class("TButton", "<Leave>", self.on_leave)
-        try:
-            user_cfg_path = None
-            normal_steam_user_cfg_location = Path(f'{os.environ["APPDATA"]}\\Microsoft Flight Simulator\\UserCfg.opt')
-            normal_msfs_store_user_cfg_location = Path(f'{os.environ["LOCALAPPDATA"]}\\Packages\\Microsoft.FlightSimulator_8wekyb3d8bbwe\\LocalCache\\UserCfg.opt')
-            if normal_steam_user_cfg_location.is_file():
-                user_cfg_path = normal_steam_user_cfg_location
-            elif normal_msfs_store_user_cfg_location.is_file():
-                user_cfg_path = normal_msfs_store_user_cfg_location
-            else:
-                try:
-                    for path in Path(Path(os.environ['APPDATA']).parent).rglob('UserCfg.opt'):
-                        clean_path = path.resolve()
-                        if "Flight" in str(clean_path) and str(clean_path).endswith('UserCfg.opt'):
-                            user_cfg_path = path
-                            break
-                except FileNotFoundError:
-                    pass
-            if not user_cfg_path:
-                raise IOError
-            file_data = open(user_cfg_path, 'r')
-            for row in file_data:
-                if "InstalledPackagesPath" in row:
-                    found_installation_path = row.split('InstalledPackagesPath')[1].lstrip().rstrip().strip('"')
-                    self.destination_folder = f'{found_installation_path}\\Community'
-                    self.destination_folder_msg['text'] = found_installation_path
-            self.browse_button['text'] = "Change destination folder"
-            self.browse_button.pack(side="top", pady=(20, 0))
-            self.browse_search()
-            file_data.close()
-        except IOError:
-            self.destination_folder = ""
-            self.response_status['text'] = "Welcome to A32NX Mod Downloader & Installer. Could not automatically detect Community folder, please select it manually!"
-            self.browse_button['text'] = "Select Community folder"
-            self.browse_button.pack(side="top", pady=(20, 0))
-        self.final_file_path = ""
-        self.master = master
-        master.title(f'FlyByWire Downloader {current_version}')
+        if "FlightSimulator.exe" not in (p.name() for p in psutil.process_iter()):
+            self.latest_release_version = ""
+            self.latest_development_update_timestamp = ""
+            self.response_status = ttk.Label(text="Welcome to A32NX Mod Downloader & Installer!", wraplength=400, background="#1B1B1B", foreground="white")
+            self.response_status.pack(side="top", fill=tkinter.X)
+            self.update_installer_label = ttk.Label(text="", background="#1B1B1B")
+            threading.Thread(target=self.check_installer_update()).start()
+            self.filler_label = ttk.Label(text="", background="#1B1B1B")
+            self.filler_label.pack(side="top", fill=tkinter.X)
+            self.destination_folder_msg = ttk.Label(text="", background="#1B1B1B", wraplength=400)
+            self.destination_folder_msg.pack(side="top", fill=tkinter.X)
+            self.progress_bar = ttk.Progressbar(self, style='text.Horizontal.TProgressbar', orient="horizontal", length=200, mode="determinate")
+            self.download_dev_btn = ttk.Button(self, text="Development version", width=20, style='W1.TButton', command=self.download_dev)
+            self.download_stable_btn = ttk.Button(self, width=20, text="Stable version", style='W2.TButton', command=self.download_stable)
+            self.browse_button = ttk.Button(self, text="", width=30, style='W3.TButton', command=self.browse_search)
+            self.exit = ttk.Button(self, text="Exit", style='W4.TButton', command=self.master.destroy)
+            self.exit.pack(side="bottom", pady=(30, 0), padx=(184, 184))
+            self.filler_label2 = ttk.Label(text="", background="#1B1B1B")
+            self.filler_label2.pack(side="bottom", fill=tkinter.X)
+            self.cancel = ttk.Button(self, text="Cancel", style='W5.TButton', command=Request.update_cancel)
+            root.bind_class("TButton", "<Enter>", self.on_enter)
+            root.bind_class("TButton", "<Leave>", self.on_leave)
+            try:
+                user_cfg_path = None
+                normal_steam_user_cfg_location = Path(f'{os.environ["APPDATA"]}\\Microsoft Flight Simulator\\UserCfg.opt')
+                normal_msfs_store_user_cfg_location = Path(f'{os.environ["LOCALAPPDATA"]}\\Packages\\Microsoft.FlightSimulator_8wekyb3d8bbwe\\LocalCache\\UserCfg.opt')
+                if normal_steam_user_cfg_location.is_file():
+                    user_cfg_path = normal_steam_user_cfg_location
+                elif normal_msfs_store_user_cfg_location.is_file():
+                    user_cfg_path = normal_msfs_store_user_cfg_location
+                else:
+                    try:
+                        for path in Path(Path(os.environ['APPDATA']).parent).rglob('UserCfg.opt'):
+                            clean_path = path.resolve()
+                            if "Flight" in str(clean_path) and str(clean_path).endswith('UserCfg.opt'):
+                                user_cfg_path = path
+                                break
+                    except FileNotFoundError:
+                        pass
+                if not user_cfg_path:
+                    raise IOError
+                file_data = open(user_cfg_path, 'r')
+                for row in file_data:
+                    if "InstalledPackagesPath" in row:
+                        found_installation_path = row.split('InstalledPackagesPath')[1].lstrip().rstrip().strip('"')
+                        self.destination_folder = f'{found_installation_path}\\Community'
+                        self.destination_folder_msg['text'] = found_installation_path
+                self.browse_button['text'] = "Change destination folder"
+                self.browse_button.pack(side="top", pady=(20, 0))
+                self.browse_search()
+                file_data.close()
+            except IOError:
+                self.destination_folder = ""
+                self.response_status['text'] = "Welcome to A32NX Mod Downloader & Installer. Could not automatically detect Community folder, please select it manually!"
+                self.browse_button['text'] = "Select Community folder"
+                self.browse_button.pack(side="top", pady=(20, 0))
+        else:
+            self.response_status = ttk.Label(text="Welcome to A32NX Mod Downloader & Installer! Please close MSFS before installing/updating the A32NX mod to avoid issues.", wraplength=400, background="firebrick", foreground="white")
+            self.response_status.pack(side="top", fill=tkinter.X)
+            self.exit = ttk.Button(self, text="Exit", style='W4.TButton', command=self.master.destroy)
+            self.exit.pack(side="bottom", pady=(30, 0), padx=(184, 184))
         self.pack(after=self.exit.pack(side="bottom", pady=(20, 0), padx=(184, 184)))
 
     def get_asset_json_path(self):
@@ -173,12 +229,19 @@ class Application(ttk.Frame):
         self.response_status['background'] = ""
         self.response_status.pack(side="top", fill=tkinter.X)
         if not self.change_folder or not self.destination_folder:
+            previous_folder = self.destination_folder
             self.destination_folder = filedialog.askdirectory()
+            if not self.destination_folder and previous_folder:
+                self.destination_folder = previous_folder
         if self.destination_folder:
             self.response_status['text'] = "Welcome to A32NX Mod Downloader & Installer!"
             threading.Thread(target=self.check_if_update_available()).start()
             self.download_dev_btn.pack(side="left", pady=(20, 0), padx=(20, 0))
             self.download_stable_btn.pack(side="right", pady=(20, 0), padx=(0, 20))
+            CreateToolTip(self.download_stable_btn, f"Latest available stable version: {self.latest_release_version}")
+            if self.latest_development_update_timestamp:
+                date, timestamp = self.latest_development_update_timestamp.split('T')
+                CreateToolTip(self.download_dev_btn, f"Latest development version was updated at: {date} {timestamp}")
             msg = f'Destination folder: {self.destination_folder}'
             self.browse_button['text'] = "Change destination folder"
             self.browse_button.pack(side="top", pady=(20, 0))
@@ -211,7 +274,7 @@ class Application(ttk.Frame):
                 file_name = download_url.split("/")[-1]
                 threading.Thread(target=Request.download_file(url=download_url, file_name=file_name, progress_bar=self.progress_bar, response_status=self.response_status, stable=stable)).start()
                 if not self.response_status['text'] and not Request.cancel_check:
-                    threading.Thread(target=self.unzip_file(file_name=file_name, stable=stable)).start()
+                    threading.Thread(target=self.unzip_file, kwargs={'file_name': file_name, 'stable': stable}).start()
                 elif Request.cancel_check or "permission denied" in self.response_status['text']:
                     if not self.response_status['text']:
                         self.response_status['text'] = f"Download cancelled!"
@@ -237,7 +300,8 @@ class Application(ttk.Frame):
                 self.update_installer_label['text'] = f"Please update installer, new version {latest_installer_version} available!"
                 self.update_installer_label['background'] = "#e85d04"
                 self.update_installer_label.pack(side="top", fill=tkinter.X)
-                browser_update_button = ttk.Button(self, text="Open new version page", style='W6.TButton', command=Request.open_installer_release_page_browser)
+                browser_update_button = ttk.Button(self, text="New Installer version", style='W6.TButton', command=Request.open_installer_release_page_browser)
+                CreateToolTip(browser_update_button, f"New available version {latest_installer_version}, click to open webpage!")
                 browser_update_button.pack()
         except KeyError:
             pass
@@ -248,16 +312,18 @@ class Application(ttk.Frame):
             if manifest_path.is_file():
                 manifest_file = open(manifest_path, 'r')
                 manifest_version = json.load(manifest_file)["package_version"]
-                latest_release_version = Request.get(latest_release_url).json()['tag_name'].strip("v")
-                if manifest_version == latest_release_version and self.get_asset_json_path().is_file():
+                self.latest_release_version = Request.get(latest_release_url).json()['tag_name'].strip("v")
+                if manifest_version == self.latest_release_version and self.get_asset_json_path().is_file():
                     with open(self.get_asset_json_path()) as file:
                         asset_data = json.load(file)
                         if 'stable' in asset_data.keys():
-                            self.filler_label['text'] = f"Stable version {latest_release_version} is up to date!"
+                            self.filler_label['text'] = f"Stable version {self.latest_release_version} is up to date!"
                             self.filler_label['background'] = "green"
                             return
                         local_asset_id = asset_data['id']
-                        latest_master_asset_id = Request.get(master_prerelease_url).json()['assets'][0]['id']
+                        development_github_data = Request.get(master_prerelease_url).json()
+                        latest_master_asset_id = development_github_data['assets'][0]['id']
+                        self.latest_development_update_timestamp = development_github_data['assets'][0]['updated_at']
                         if local_asset_id == latest_master_asset_id:
                             self.filler_label['text'] = "Development version is up to date!"
                             self.filler_label['background'] = "green"
@@ -303,9 +369,9 @@ class Application(ttk.Frame):
                 json.dump(latest_master_asset, f, ensure_ascii=False, indent=4)
             self.response_status['background'] = "green"
             self.response_status['text'] = "A32NX correctly installed, everything should be good to go!"
-        except (zipfile.BadZipfile, OSError, KeyError):
+        except (zipfile.BadZipfile, OSError, KeyError) as e:
             self.response_status['background'] = "firebrick"
-            self.response_status['text'] = "Folder could not be unzipped, failed!"
+            self.response_status['text'] = f"Folder could not be unzipped, failed reason: {repr(e)}!"
         self.exit.pack(side="bottom", pady=(20, 0), padx=(184, 184))
         self.cancel.pack_forget()
         self.pack()
